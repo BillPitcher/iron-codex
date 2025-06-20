f3e7c412b6c64dfbdbcb209bb21b0fdd1f0f02ce
Bill Pitcher <bill@ilike.co.nz>
2025-06-20 15:52:59 +1000

    Added the Turnmarkers

--- scripts/features/swapTurnMarker.js
+++ scripts/features/swapTurnMarker.js
@@ -3,10 +3,10 @@ const CLIENT_KEY = "enableSwapTurnMarker";
 
 export async function init()  {
     if (game.settings.get(MODULE_ID, CLIENT_KEY)) {
-        if (CONFIG.Combat.fallbackTurnMarker === 'modules/iron-codex/artwork/fm-turnmarker-disposition') {
+        if (CONFIG.Combat.fallbackTurnMarker === 'modules/iron-codex/artwork/fm-turnmarker-disposition.png') {
             CONFIG.Combat.fallbackTurnMarker = 'icons/vtt-512.png'
         } else {
-            CONFIG.Combat.fallbackTurnMarker = 'modules/iron-codex/artwork/fm-turnmarker-disposition'
+            CONFIG.Combat.fallbackTurnMarker = 'modules/iron-codex/artwork/fm-turnmarker-disposition.png'
             const combat = game.combat;
             if (!combat) return;
             const current = combat.combatant;
