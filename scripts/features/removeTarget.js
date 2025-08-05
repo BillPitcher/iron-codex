e4cd153836924884777e5085968617130ebce118
Bill Pitcher <bill@ilike.co.nz>
2025-08-05 14:09:13 +1000

    2.0.1 Remove Target does less checking, deals with client delay better.

--- scripts/features/removeTarget.js
+++ scripts/features/removeTarget.js
@@ -1,17 +1,8 @@
 // features/removeTarget.js
-// v2.0.0
+// v2.0.1
 export function init() {
     Hooks.on("combatTurnChange", (combat, prior, current) => {
         if (!game.user.isGM) return;
-
-        const currentCombatant = combat.combatant;
-        if (!currentCombatant) return;
-
-        const actor = currentCombatant.actor;
-        if (!actor || actor.hasPlayerOwner) return; // Only trigger for NPCs
-
-        const npcToken = currentCombatant.token?.object;
-        if (!npcToken) return;
 
         // For each just GM, remove targets on PCs (by ownership or actor type)
         for (const target of game.user.targets) {
